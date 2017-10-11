f1d64515f409b424416bfc8cd84327104e546aeb
Maria Paula Ferraz <MariaPaula@Marias-MacBook-Pro.local>
2017-10-11 13:10:38 -0300

    classe otsu -mp

--- src/main/java/com/poli/model/segmentation/OtsuThreshold.java
+++ src/main/java/com/poli/model/segmentation/OtsuThreshold.java
@@ -1,127 +1,90 @@
 package com.poli.model.segmentation;
 
 import java.util.HashMap;
-import java.util.Map.Entry;
 
 import com.poli.model.Image;
 
 public class OtsuThreshold
 {
 
-    private HashMap<Integer, Integer> pixelDistribution;
-    private Image image;
+	private HashMap<Integer, Integer> pixelDistribution;
+	private Image image;
 
-    public OtsuThreshold(Image image)
-    {
-        this.image = image;
-        this.generateImageHistogram();
-    }
+	public OtsuThreshold(Image image)
+	{
+		this.image = image;
+		this.generateImageHistogram();
+	}
 
-    private void generateImageHistogram()
-    {
+	private void generateImageHistogram()
+	{
 
-        this.pixelDistribution = new HashMap<Integer, Integer>();
+		this.pixelDistribution = new HashMap<Integer, Integer>();
 
-        for (int i = 0; i < this.image.getRows(); i++)
-        {
-            for (int j = 0; j < this.image.getCols(); j++)
-            {
-                int currentPixel = this.image.getPixel(i, j);
+		for (int i = 0; i < 256; i++)
+		{
+			this.pixelDistribution.put(i, 0);
+		}
 
-                if (this.pixelDistribution.containsKey(currentPixel))
-                {
-                    int value = this.pixelDistribution.get(currentPixel);
-                    this.pixelDistribution.put(currentPixel, ++value);
-                }
-                else
-                {
-                    this.pixelDistribution.put(currentPixel, 1);
-                }
+		for (int i = 0; i < this.image.getRows(); i++)
+		{
+			for (int j = 0; j < this.image.getCols(); j++)
+			{
+				int currentPixel = this.image.getPixel(i, j);
 
-            }
-        }
-    }
+				int value = this.pixelDistribution.get(currentPixel);
+				this.pixelDistribution.put(currentPixel, ++value);
 
-    public int getOtsuThreshold()
-    {
-        int threshHold = 0;
+			}
+		}
+	}
 
-        int totalPixel = this.image.getCols() * this.image.getRows();
+	public int getOtsuThreshold()
+	{
+		int threshHold = 0;
 
-        double maxVariance = 0;
+		int totalPixel = this.image.getCols() * this.image.getRows();
+		int totalSum = 0;
 
-        for (int t = 0; t < 256; t++)
-        {
+		for (int i = 0; i < 256; i++)
+		{
+			totalSum += i * this.pixelDistribution.get(i);
+		}
 
-            double weightBackground = 0;
-            double weightForeground = 0;
+		double weightBackground = 0;
+		double weightForeground = 0;
+		double maxVariance = 0;
+		double currentSum = 0;
 
-            double meanBackground = 0;
-            double meanForeground = 0;
+		for (int t = 0; t < 256; t++)
+		{
+			weightBackground += this.pixelDistribution.get(t);
 
-            double meanBackDiv = 0;
-            double meanForeDiv = 0;
+			if (weightBackground > 0)
+			{
+				currentSum += (double) t * this.pixelDistribution.get(t);
 
-            if (!this.pixelDistribution.containsKey(t))
-            {
-                continue;
-            }
+				weightForeground = (totalPixel - weightBackground);
 
-            if (t > 0)
-            {
-                for (int i = 0; i < t; i++)
-                {
-                    weightBackground += this.pixelDistribution.get(i);
-                    meanBackground += (double) this.getKeyAsIndex(i) * this.pixelDistribution.get(i);
-                    meanBackDiv += this.pixelDistribution.get(i);
-                }
+				if (weightForeground == 0)
+				{
+					break;
+				}
+				double meanBackground = currentSum / weightBackground;
+				double meanForeground = (totalSum - currentSum) / weightForeground;
 
-                weightBackground /= totalPixel;
-                meanBackground /= meanBackDiv;
-            }
-            if (t < 256)
-            {
-                for (int i = t; i < 256; i++)
-                {
-                    weightForeground += this.pixelDistribution.get(i);
-                    meanForeground += (double) this.getKeyAsIndex(i) * this.pixelDistribution.get(i);
-                    meanForeDiv += this.pixelDistribution.get(i);
-                }
+				double beetweenVariance = weightBackground * weightForeground
+						* Math.pow(meanBackground - meanForeground, 2);
 
-                weightBackground /= totalPixel;
-                meanBackground /= meanForeDiv;
-            }
+				if (beetweenVariance > maxVariance)
+				{
+					maxVariance = beetweenVariance;
+					threshHold = t;
+				}
+			}
+		}
 
-            double beetweenVariance = weightBackground * weightForeground
-                    * Math.pow(meanBackground - meanForeground, 2);
-
-            if (beetweenVariance > maxVariance)
-            {
-                maxVariance = beetweenVariance;
-                threshHold = t;
-            }
-        }
-
-        return threshHold;
-    }
-
-    private Integer getKeyAsIndex(int i)
-    {
-        Integer key = 0;
-
-        int index = 0;
-
-        for (Entry<Integer, Integer> element : this.pixelDistribution.entrySet())
-        {
-            if (index == i)
-            {
-                key = element.getKey();
-                break;
-            }
-            index++;
-        }
-
-        return key;
-    }
+		return threshHold;
+	}
 
 }
